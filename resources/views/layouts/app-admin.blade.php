ead88478edc090c9f76a138281ca6f98d818160c
Nugee <nugrohorahmanto24@gmail.com>
2025-10-27 22:56:43 +0700

    add flag

--- resources/views/layouts/app-admin.blade.php
+++ resources/views/layouts/app-admin.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PJRVZT7CVP"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PJRVZT7CVP');
+    </script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title','Admin')</title>
